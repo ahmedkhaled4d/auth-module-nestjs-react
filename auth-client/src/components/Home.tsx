444917a19f7972c4a0848cdaf015f81362856a0c
ahmedkhaled4d <ahmedkhaled4d@gmail.com>
2023-11-20 16:15:30 +0200

    add auth , application components

---
+++ auth-client/src/components/Home.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function (props) {
+  return (
+    <div className="Auth-form-container">
+      <h2>Welcome to the application.</h2>
+    </div>
+  );
+}
